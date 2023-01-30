57a0c89d5951d0ca1ec49ffc4df70573a1005e33
Kartik <abheytyagi010@gmail.com>
2023-01-30 20:47:19 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ const expresslayout = require('express-ejs-layouts')
 
 app.use(express.static('public'))
 
+app.use(expresslayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+
 app.get('/', function(req,res) {
     res.render('home')
 })
 
 
-app.use(expresslayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', function(req,res) {
+    res.render('customers/cart')
+})
+
+app.get('/login', function(req,res) {
+    res.render('auth/login')
+})
+
+
+app.get('/register', function(req,res) {
+    res.render('auth/register')
+})
 
 app.listen(3000 , () => {
     console.log('port 3000')
